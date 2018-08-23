eff179e95c4f4c801969aef5fa708836e8acc681
xsiasu <xsiasu@gmail.com>
2018-08-23 11:33:06 +0900

    success

--- src/component/Clicker.js
+++ src/component/Clicker.js
@@ -11,7 +11,7 @@ class Clicker extends Component {
 
     this.incrementCount = this.incrementCount.bind(this);
     this.decrementCount = this.decrementCount.bind(this);
-    this.resetCount = this.resetCount(this);
+    this.resetCount = this.resetCount.bind(this);
   }
 
 
@@ -32,21 +32,22 @@ class Clicker extends Component {
 
   render() {
     return(
+
       <div className="container">
           <div className="clicker border border-secondary rounded">
               <div className="clicker-display d-flex align-items-center bg-light text-secondary">
                   <div className="mx-auto display-1">{this.state.count}</div>
               </div>
               <div className="clicker-button-panel d-flex flex-row">
-                  <button className="btn btn-success w-100" onClick={this.incrementCount}>
-                      <i className="fa fa-plus fa-2x"></i>
-                  </button>
-                  <button className="btn btn-warning w-100" onClick={this.resetCount}>
-                      <i className="fa fa-refresh fa-2x"></i>
-                  </button>
                   <button className="btn btn-danger w-100" onClick={this.decrementCount}>
                       <i className="fa fa-minus fa-2x"></i>
                   </button>
+                  <button className="btn btn-warning w-100" onClick={this.resetCount}>
+                      <i className="fa fa-refresh fa-2x"></i>
+                  </button>                  
+                  <button className="btn btn-success w-100" onClick={this.incrementCount}>
+                      <i className="fa fa-plus fa-2x"></i>
+                  </button>                  
               </div>
           </div>
       </div>
